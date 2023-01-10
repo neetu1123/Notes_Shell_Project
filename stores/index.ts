5badd927e5d2efd25a4b14cedf371e262d0c838a
neetu_chaurasiya <neetu@gma8il.com>
2023-01-10 10:23:11 +0530

    add setter

--- stores/index.ts
+++ stores/index.ts
@@ -5,17 +5,17 @@ const useCounterStore = defineStore("counter", () => {
   const notesLists = ref([]);
 
   // push the data 
-  function ADD_LIST(title, description) {
+  function ADD_LIST(title:String, description:string) {
     console.log(title, description);
     notesLists.value.push({ title, description });
   }
 
-  function getNoteByindex(index) {
+  function getNoteByindex(index:number) {
     return notesLists.value[index - 1];
  }
 // filter the data
   const filterData = computed(() => {
-    const getData = notesLists.value.filter((ele) => ele.title != title.value);
+    const getData = notesLists.value.filter((ele,index) => ele[index]);
   });
 
   // return properties
